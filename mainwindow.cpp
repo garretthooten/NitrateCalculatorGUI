2e8e49bf5da77484dbdfd4f076969cc595f7af4c
Garrett <garrett@Garretts-Air.hsd1.tn.comcast.net>
2021-05-06 12:53:47 -0500

    Implementing first layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+std::vector< std::vector<int> >MainWindow::parse_CSV(std::string directory)
+{
+    std::ifstream data(directory);
+    data.exceptions(std::ifstream::badbit);
+    std::vector< std::vector<std::string> > parsed_String_CSV;
+    bool done = false;
+    while(!done)
+    {
+        if(data.is_open())
+            done = true;
+        else
+    }
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -14,9 +28,9 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_beginButton_clicked()
+
+void MainWindow::on_TravelTimeFileButton_clicked()
 {
-    //Create File Dialog
-    QString filePath = QFileDialog::getOpenFileName(this, "Select a folder", "");
-    ui->textEdit->setText("You have selected: " + filePath);
+    QString tt_filepath = QFileDialog::getOpenFileName(this, "Open a travel time .csv");
+    QString yearString = ui->YearEntry->toPlainText();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include <QFileDialog>
+#include <string>
+#include <vector>
+#include <fstream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,8 +19,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    std::vector< std::vector<int> > parse_CSV(std::string directory);
+
 private slots:
-    void on_beginButton_clicked();
+
+    void on_TravelTimeFileButton_clicked();
 
 private:
     Ui::MainWindow *ui;
